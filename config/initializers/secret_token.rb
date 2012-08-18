22093ef4d3c20e590d0e9ed3bfc39f870e83b865
Luke Reimer <reimerluke@yahoo.com>
2012-08-18 17:17:27 -0400

    Initial commit for history places project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HistoryPlaces::Application.config.secret_token = '259d9fa3be05ee9e5f4555ee2303de0cd8cf66e3427ac9c690ba3be633cfb9ec6ce8b724337e1b297777056dad17724662fb6fe177af5824cdd7d8bb0661f337'
